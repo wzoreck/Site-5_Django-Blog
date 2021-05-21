00b2ee7d9905e121f84a8fdd421fc8c2774fb54e
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-05-21 11:25:24 -0300

    Exibindo template inicial e trazendo os objetos Post para o frontend

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from .models import Post
 
 # Create your views here.
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 3 # Número de objetos que irão aparecer por página
+    context_object_name = 'posts' # Nomeando o nome da 'lista' que vai ser passada para o template, no for apresentamos cada post
 
 class PostBusca(PostIndex):
     pass
